d24b14e5ba56bc403b74259a219f905367647d57
erinnwhittyxx <erinnwhittyxx@gmail.com>
2020-12-31 16:40:25 +0000

    Week 9 Users Router

--- movies_api/index.js
+++ movies_api/index.js
@@ -1,5 +1,6 @@
 import './db';
 import {loadUsers} from './seedData'
+import usersRouter from './api/users';
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
@@ -26,6 +27,7 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use('/api/movies', moviesRouter);
+app.use('/api/users', usersRouter);
 app.use(errHandler);
 
 app.listen(port, () => {
